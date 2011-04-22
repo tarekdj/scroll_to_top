1afffdf10f99c5517e3afac6e59c270c71a6e36c
Djebali Tarek <tarekdj@gmail.com>
2011-04-22 13:42:11 +0100

    Initial commit.

---
+++ scroll_to_top.js
@@ -0,0 +1,20 @@
+	// fade in #back-top
+	$(function () {
+		// hide #back-top first
+		$("#back-top").hide();
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('#back-top').fadeIn();
+			} else {
+				$('#back-top').fadeOut();
+			}
+		});
+
+		// scroll body to 0px on click
+		$('#back-top a').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
