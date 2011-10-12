14c9f1377dfc7717dc44b6d5314d62d3e3011797
tarekdj <tarekdj@gmail.com>
2011-10-12 16:55:06 +0100

    using jquery append function to make back to top link appears in all pages

--- scroll_to_top.js
+++ scroll_to_top.js
@@ -2,6 +2,8 @@
  * Scroll to top behavior
  */
 Drupal.behaviors.scroll_to_top = function (context) {
+		// append  back to top link top body
+		$("body").append("<p id='back-top'><a href='#top'><span></span>"+Drupal.t("Back to Top")+"</a></p>");
 		// hide #back-top first
 		$("#back-top").hide();
 		$(window).scroll(function () {
@@ -18,5 +20,6 @@ Drupal.behaviors.scroll_to_top = function (context) {
 				scrollTop: 0
 			}, 800);
 			return false;
-		});
+		}); 
 };
+
