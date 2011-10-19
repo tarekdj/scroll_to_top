cf0a4dd0a995fbd6935f248fbfc3d009831bb29e
Djebali Tarek <tarekdj@gmail.com>
2011-10-19 16:00:38 +0100

    adding display label option

--- scroll_to_top.js
+++ scroll_to_top.js
@@ -3,7 +3,7 @@
  */
 Drupal.behaviors.scroll_to_top = function (context) {
 		// append  back to top link top body
-		$("body").append("<p id='back-top'><a href='#top'><span></span>"+Drupal.t("Back to Top")+"</a></p>");
+		$("body").append("<p id='back-top'><a href='#top'><span></span><div id='link'>"+Drupal.t("Back to Top")+"</div></a></p>");
 		// hide #back-top first
 		$("#back-top").hide();
 		$(window).scroll(function () {
